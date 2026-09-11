48582c35d6229c4640be398b3a8dd49d23633ff9
Gqtien <bin.gatien@gmail.com>
2026-09-11 18:29:30 +0200

    feat: open fullscreen tkinter window

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,17 @@
 import sys
+from tkinter import Tk
+
+
+def window_example() -> None:
+    tk = Tk()
+    tk.title("Pac-Man")
+    w, h = tk.winfo_screenwidth(), tk.winfo_screenheight()
+    tk.geometry(f"{w}x{h}")
+    tk.mainloop()
+
 
 if __name__ == "__main__":
     try:
-        ...
+        window_example()
     except Exception as exc:
         sys.exit(f"Error: {exc}")
