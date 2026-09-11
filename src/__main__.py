8dcca2077a14a88384d8c688103ac99970e83130
Gqtien <bin.gatien@gmail.com>
2026-09-11 21:58:50 +0200

    feat: launch scene manager

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,13 @@
 import sys
-from tkinter import Tk
+from scenes import SceneManager
 
 
-def window_example() -> None:
-    tk = Tk()
-    tk.title("Pac-Man")
-    w, h = tk.winfo_screenwidth(), tk.winfo_screenheight()
-    tk.geometry(f"{w}x{h}")
-    tk.mainloop()
+def run() -> None:
+    SceneManager()
 
 
 if __name__ == "__main__":
     try:
-        window_example()
+        run()
     except Exception as exc:
         sys.exit(f"Error: {exc}")
