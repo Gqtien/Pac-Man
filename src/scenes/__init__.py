008923e217de147cd65535df6e9ae710ae97136e
Gqtien <bin.gatien@gmail.com>
2026-09-13 19:13:00 +0200

    chore(scenes): export transition

--- src/scenes/__init__.py
+++ src/scenes/__init__.py
@@ -2,5 +2,6 @@ from .base import Scene
 from .main import Main
 from .gameplay import Gameplay
 from .manager import SceneManager
+from .transition import Transition
 
-__all__ = ["Scene", "SceneManager", "Main", "Gameplay"]
+__all__ = ["Scene", "SceneManager", "Main", "Gameplay", "Transition"]
