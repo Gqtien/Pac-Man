5f2ba4610d9b5a745716513769dfaac5e4d70b3e
purple <marcel.phippen@proton.me>
2026-09-20 17:36:04 +0200

    feat: base64 encode saved highscore

--- src/systems/highscore.py
+++ src/systems/highscore.py
@@ -1,14 +1,20 @@
 from pathlib import Path
-import json
+import base64
 
 def load_highscore(filepath: str) -> int:
     path = Path(filepath)
     if not path.exists():
         save_highscore(0, filepath)
-    highscore: int = json.loads(path.read_text())
+    try:
+        encoded_buffer: bytes = path.read_bytes()
+        highscore: int = int(base64.b64decode(encoded_buffer).decode())
+    except (OSError, ValueError):
+        save_highscore(0, filepath)
+        return 0
     return highscore
 
 
 def save_highscore(highscore: int, filepath: str) -> None:
     path = Path(filepath)
-    path.write_text(json.dumps(highscore))
+    encoded_buffer: bytes = str(highscore).encode()
+    path.write_bytes(base64.b64encode(encoded_buffer))
